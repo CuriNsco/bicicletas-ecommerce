8c7ed8b8d6bd039584bcf6dc504179d83674d43b
CuriNsco <curiagus99@gmail.com>
2023-07-20 17:27:48 -0300

    configuracion de tailwind, componente hero creado y aplicado de globals css

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>
+                Find, book, or rent a car - quickly and easily
+            </h1>
+            <p className='hero__subtitle'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus ipsam veritatis error tempore accusantium, corporis eius nobis ipsum debitis necessitatibus soluta. Nulla ipsam placeat mollitia illo ipsa culpa id ea.</p>
+        </div>
+    </div>
   )
 }
 
