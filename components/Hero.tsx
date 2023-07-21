c8c6dbe677ae2362812f5ed4f24c43a5498f5411
CuriNsco <curiagus99@gmail.com>
2023-07-20 21:22:01 -0300

    anadi nuevos svg que voy a utilizar y crearcion de button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,15 @@
+'use client'
+
+import Image from 'next/image'
 import React from 'react'
+import CustomButton from './CustomButton'
 
 const Hero = () => {
+
+    const handleScroll = ()=>{
+
+    }
+    
   return (
     <div className='hero'>
         <div className='flex-1 pt-36 padding-x'>
@@ -8,6 +17,18 @@ const Hero = () => {
                 Find, book, or rent a car - quickly and easily
             </h1>
             <p className='hero__subtitle'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus ipsam veritatis error tempore accusantium, corporis eius nobis ipsum debitis necessitatibus soluta. Nulla ipsam placeat mollitia illo ipsa culpa id ea.</p>
+            
+            <CustomButton
+            title='Explore Bikes'
+            containerStyles = 'bg-primary-blue text-white rounded-full mt-10'
+            handleClick={handleScroll}
+            />
+        </div>
+        <div className='hero__image-container'>
+            <div className='hero__image'>
+                <Image src='' alt=''/>
+            </div>
+
         </div>
     </div>
   )
